360cd2a11e919d5f2f21827aa2eb49d38e43731f
BacancyAnkita <ankita.rathod@bacancytechnology.com>
2017-02-28 13:11:14 +0530

    Experience form editted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,24 @@ class UsersController < ApplicationController
       render "index"
     end
   end
+  def edit
+     @user = User.find_by(id: params[:id])
+  end
+  def update
+    @user = User.find_by(id: params[:id])
+
+    if @user.update(users_param)
+      redirect_to users_path
+    else
+      render "edit"
+    end
+  end
+  def destroy
+    @user=User.find_by(id: params[:id])
+    if @user.destroy
+      redirect_to users_url
+    end
+  end
   private
   def users_param
     params.require(:user).permit(:designation,:company,:working_since,:location,:notice,:months)
